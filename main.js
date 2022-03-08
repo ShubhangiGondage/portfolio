bd20edbbc1548bb030fc6dc3d1e1da03923cad5c
Shubhangi Gondage <78039310+ShubhangiGondage@users.noreply.github.com>
2022-03-08 20:50:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 30) {
+            $('.navbar').addClass('sticky');
+        }
+        else {
+            $('.navbar').removeClass('sticky');
+        }
+
+        // Scroll up button
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // toggle menu baar
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+    })
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+// slide-up script
+$('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $('html').css("scrollBehavior", "auto");
+});
+
+$('.navbar .menu li a').click(function () {
+    // applying again smooth scroll on menu items click
+    $('html').css("scrollBehavior", "smooth");
+});
+// SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll("section[class]");
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        let sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector(".nav_menu a[href*=" + sectionId + "]")
+                .classList.add("active-link");
+        } else {
+            document
+                .querySelector(".nav_menu a[href*=" + sectionId + "]")
+                .classList.remove("active-link");
+        }
+    });
+}
+window.addEventListener("scroll", scrollActive);
+// onsubmit function for display message after submitting the contact form
+function sub() {
+    alert("Your message has been sent successfully!!!");
+}
